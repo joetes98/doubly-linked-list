b3e5dbea32ffe7d22f35713687aed43f77141534
joetes98 <joseph.tesoriero@ucdenver.edu>
2024-10-14 19:13:46 -0400

    WIP

--- Assignment 2/List.cpp
+++ Assignment 2/List.cpp
@@ -40,14 +40,75 @@ void List::insertAtIndex(int data, int index){
     }
 
     Node *newNode = new Node(data, current, prev);
-    prev->setNextPtr(new Node);
+    prev->setNextPtr(newNode);
+    current->setPrevPtr(newNode);
 }
-void List::deleteAtIndex(int index)
-{}
-void List::readItem(int index){}
-void List::reverseList(){}
 
-void List::printForward()
-{}
-void List::printBackward()
-{}
+void List::deleteAtIndex(int index){
+
+    Node *current = headPtr->getNextPtr();
+    Node *prev = headPtr;
+
+    int i = 0;
+
+    while (current != NULL){
+        if (i = index)
+            break;
+        prev = current;
+        current = current->getNextPtr();
+        i++;
+    }
+    Node *nextPtr = current->getNextPtr();
+    prev->setNextPtr(current->getNextPtr());
+    nextPtr->setPrevPtr(prev);
+}
+
+void List::readItem(int index){
+
+    Node *current = headPtr->getNextPtr();
+
+    int i = 0;
+
+    while (current != NULL){
+        if (i == index)
+            cout << current->getData() << endl;
+            return;
+        current = current->getNextPtr();
+        i++;
+    }
+    cout <<  current->getData() << endl;
+}
+
+void List::reverseList(){
+
+    Node *frontPtr = headPtr->getNextPtr();
+    Node *backPtr = tailPtr->getPrevPtr();
+
+    while(frontPtr != NULL){
+        if (frontPtr == backPtr)
+            break;
+        Node *temp1 = backPtr;
+        backPtr->setNextPtr(frontPtr->getNextPtr());
+        frontPtr->setNextPtr(temp1->getPrevPtr());
+    }
+}
+
+void List::printForward(){
+
+    Node *current = headPtr->getNextPtr();
+
+    while(current != NULL){
+        cout << current->getData() << endl;
+        current = current->getNextPtr();
+    }
+}
+
+void List::printBackward(){
+
+    Node *current = tailPtr->getPrevPtr();
+
+    while(current!= NULL && current->getPrevPtr() != NULL){
+        cout << current->getData() << endl;
+        current = current->getPrevPtr();
+    }
+}
